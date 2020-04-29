cb416b9fba2724891c66a9cb4d84a65bafc44d5d
Jeremy Soller <jeremy@system76.com>
2020-04-29 13:27:54 -0600

    4.0.3 - Add LTS to version for 18.04, 20.04, and 22.04

--- src/release.sh
+++ src/release.sh
@@ -2,7 +2,14 @@
 
 source /etc/os-release
 NAME="Pop!_OS"
-VERSION="${VERSION_ID}"
+case "${VERSION_ID}" in
+    18.04|20.04|22.04)
+        VERSION="${VERSION_ID} LTS"
+        ;;
+    *)
+        VERSION="${VERSION_ID}"
+        ;;
+esac
 ID="pop"
 ID_LIKE="ubuntu debian"
 PRETTY_NAME="${NAME} ${VERSION}"

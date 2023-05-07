c84013c7ce75e4b6a0c2acb9e9ab4b2a208eb9c5
Jacob Kauffmann <jacob@system76.com>
2023-05-06 21:09:52 -0600

    enhance: Disable various Firefox annoyances

--- usr/lib/firefox/browser/defaults/preferences/pop-default-settings.js
+++ usr/lib/firefox/browser/defaults/preferences/pop-default-settings.js
@@ -1,1 +1,9 @@
+// Turn off Pocket-recommended articles and advertisements on the New Tab page.
 pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+
+// Turn off Mozilla survey notifications.
+pref("app.normandy.enabled", false);
+
+// Turn off multi-page "Thank you for loving Firefox" survey.
+pref("browser.aboutwelcome.enabled", false);
+//pref("trailhead.firstrun.didSeeAboutWelcome", true);
